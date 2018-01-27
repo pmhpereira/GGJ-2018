5989fce7e44b11abd0fa0f3547576602739fed0b
Pedro Lucas <pmslucas@gmail.com>
2018-01-27 00:52:09 +0000

    Input stuff. Some game data.

---
+++ Assets/Scripts/GameData/GameData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Data {
+
+    [Serializable]
+    public class Player {
+
+        public float itemSpeedModifier = 0.75f; // 3/4 speed when carrying shit
+
+        public bool hasItem { get { return m_HasItem; }}
+        public int playerIndex { get { return m_PlayerIndex; } }
+        public float life { get { return m_Life; }}
+        public float speed { get { return hasItem ? m_Speed * itemSpeedModifier : m_Speed; }}
+
+        private bool m_HasItem;
+        private int m_PlayerIndex;
+        private float m_Life; //0f - 1f
+        private float m_Speed;
+
+        public Player(int index, float life, float speed){
+            m_Life = life;
+            m_PlayerIndex = index;
+            m_Speed = speed;
+        }
+
+
+    }
+
+    public class Game
+    {
+
+
+        public Game(){
+            
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Input/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private Data.Player m_Player; //get speed from here, theoretically, it returns the correct speed
+
+    public void Init(Data.Player player){
+        m_Player = player;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_Player == null)
+            return;
+        
+        _HandleInput();
+	}
+
+    private void _DebugInput(KeyCode k){
+        Debug.LogFormat("Player {0} pressed {1}", k.ToString());   
+    }
+
+    private void _HandleInput(){
+        if (m_Player.playerIndex == 0)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                _DebugInput(KeyCode.LeftArrow);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                _DebugInput(KeyCode.RightArrow);
+
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                _DebugInput(KeyCode.A);
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _DebugInput(KeyCode.S);
+
+            }
+        }
+        else if (m_Player.playerIndex == 1)
+        {
+
+            //BUTTONS
+            if (Input.GetButtonDown("PS4_X"))
+            {
+                print("X");
+            }
+
+            if (Input.GetButtonDown("PS4_O"))
+            {
+                print("O");
+            }
+
+            if (Input.GetButtonDown("PS4_Triangle"))
+            {
+                print("Triangle");
+            }
+
+            if (Input.GetButtonDown("PS4_Square"))
+            {
+                print("Square");
+            }
+
+            //LEFT STICK
+            if (Input.GetAxis("PS4_L_Horizontal") > 0.1)
+            {
+                print("Left Stick Right");
+            }
+
+            if (Input.GetAxis("PS4_L_Horizontal") < -0.1)
+            {
+                print("Left Stick Left");
+            }
+
+            if (Input.GetAxis("PS4_L_Vertical") < -0.1)
+            {
+                print("Left Stick Up");
+            }
+
+            if (Input.GetAxis("PS4_L_Vertical") > 0.1)
+            {
+                print("Left Stick Down");
+            }
+
+            //D-PAD
+            if (Input.GetAxis("PS4_D_Y") == 1)
+            {
+                print("D-Pad Up");
+            }
+
+            if (Input.GetAxis("PS4_D_Y") == -1)
+            {
+                print("D-Pad Down");
+            }
+            if (Input.GetAxis("PS4_D_X") == 1)
+            {
+                print("D-Pad Right");
+            }
+
+            if (Input.GetAxis("PS4_D_X") == -1)
+            {
+                print("D-Pad Left");
+            }
+
+        }
+    }
+
+}
