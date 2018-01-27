094671c8b03eec10f962b65159f6dcd8538b1162
Pedro Lucas <pmslucas@gmail.com>
2018-01-27 11:04:57 +0000

    Added first version of HUD

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour {
+
+    [SerializeField]
+    private Slider player1Life;
+    [SerializeField]
+    private Slider player2Life;
+
+    private Data.Player[] m_Players;
+
+    public void Init(float maxLife, Data.Player[] players){
+        m_Players = players;
+        player1Life.maxValue = player2Life.maxValue = maxLife;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(m_Players != null){
+
+            foreach(var p in m_Players){
+
+                if (p.playerIndex == 0)
+                    player1Life.value = p.life;
+                else
+                    player2Life.value = p.life;
+            }
+        }
+
+	}
+}
