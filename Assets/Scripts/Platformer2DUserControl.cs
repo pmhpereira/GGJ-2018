55f10a5d45bd6174bfe3a1687fb7f383517f89dd
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2018-01-27 02:08:57 +0000

    Added 2-player controls

---
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityStandardAssets._2D;
+using UnityStandardAssets.CrossPlatformInput;
+
+// Adapted from UnityStandardAssets._2D.Platformer2DUserControl
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class Platformer2DUserControl : MonoBehaviour
+{
+	public string JumpBind = "Jump";
+	public string HorizontalBind = "Horizontal";
+
+	private PlatformerCharacter2D m_Character;
+	private bool m_Jump;
+
+	private void Awake()
+	{
+		m_Character = GetComponent<PlatformerCharacter2D>();
+	}
+
+
+	private void Update()
+	{
+		if (!m_Jump)
+		{
+			// Read the jump input in Update so button presses aren't missed.
+			m_Jump = CrossPlatformInputManager.GetButtonDown(JumpBind);
+		}
+	}
+
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		bool crouch = Input.GetKey(KeyCode.LeftControl);
+		float h = CrossPlatformInputManager.GetAxis(HorizontalBind);
+		// Pass all parameters to the character control script.
+		m_Character.Move(h, crouch, m_Jump);
+		m_Jump = false;
+	}
+}
