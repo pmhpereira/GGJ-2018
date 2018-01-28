0353674d23e9ad4c39283181ab000321621a638e
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2018-01-28 14:26:37 +0000

    Sem medos!

--- Assets/Scripts/Interaction/Altar.cs
+++ Assets/Scripts/Interaction/Altar.cs
@@ -1,12 +1,31 @@
-﻿public class Altar : Interactee
-{
-	public override void Activated()
-	{
+﻿using UnityEngine;
 
+public class Altar : Handle
+{
+	public override bool NeedsManualTrigger { get { return false; } }
+
+	public bool Activated = false;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (Activated)
+			return;
+
+		if (!other.GetComponent<PlayerController>().player.hasItem)
+			return;
+
+		Activated = true;
+		OnActivated.Invoke();
+		other.GetComponent<PlayerController>().player.hasItem = false;
 	}
 
-	public override void Deactivated()
+	void OnTriggerStay2D(Collider2D other)
 	{
-
+		if (other.GetComponent<PlayerController>().player.hasItem && !Activated)
+		{
+			Activated = true;
+			OnActivated.Invoke();
+			other.GetComponent<PlayerController>().player.hasItem = false; 
+		}
 	}
 }
